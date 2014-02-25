d9fd665be1ac76a7dbfd5903448a876e6d6063ca
Kyle Cho <KyleC@Kyles-MacBook-Air.local>
2014-02-24 23:12:17 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etsydemo2::Application.config.secret_key_base = '5a84225e9a2c89f865c5c129e4baee94fd143ca2f82fad391c26a83876bb1f318ae7965af3e69fcea18ee2d8f579b6cbf25adceed1ac9f31ad5dcae27f0415ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Etsydemo2::Application.config.session_store :cookie_store, key: '_etsydemo2_session'
